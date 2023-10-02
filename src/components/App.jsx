98f03525250000aa910ebdb1cc2f39cb2907a457
MehraAnkit <am877826@gmail.com>
2023-10-02 13:28:55 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Notes"));
+    if (data != null) {
+      setNotes(data);
+    }
+    // console.log("data= " + data);
+  }, []);
+
+  // function updatedNote() {
+  //   localStorage.setItem("Notes", JSON.stringify(notes));
+  // }
+  function addNote(newNote) {
+    if (newNote.title !== "" && newNote.content !== "") {
+      setNotes(prevNotes => {
+        localStorage.setItem("Notes", JSON.stringify([...prevNotes, newNote]));
+        return [...prevNotes, newNote];
+      });
+    }
+
+  }
+
+  function deleteNote(id) {
+
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+    // updatedNote();
+    setNotes((prev) => {
+      localStorage.setItem("Notes", JSON.stringify(prev));
+      return prev;
+    })
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
